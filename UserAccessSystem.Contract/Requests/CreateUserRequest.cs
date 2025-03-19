02f69435fef61c30763bfd040ffeef56cd02bcdc
Raymond <raywig23@gmail.com>
2025-03-19 05:15:12 +0200

    Add request object

---
+++ UserAccessSystem.Contract/Requests/CreateUserRequest.cs
@@ -0,0 +1,7 @@
+namespace UserAccessSystem.Contract.Requests;
+
+public record CreateUserRequest
+{
+    public required string Username { get; set; }
+    public required string Email { get; set; }
+}
